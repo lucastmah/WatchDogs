aab1d4a7d29092f3287d8f29d78bcfdb3423f9c7
lucas <lucasmah@outlook.com>
2025-03-26 12:34:34 -0700

    fix send mail build issues

--- app/src/sendMail.c
+++ app/src/sendMail.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
+struct upload_status {
+    size_t bytes_read;
+};
+
 static const char googleSSL[] = "smtp://smtp.gmail.com:587";
 
 static const char *payload_text[] = {
@@ -10,20 +14,34 @@ static const char *payload_text[] = {
 };
 
 static size_t payload_source(void *ptr, size_t size, size_t nmemb, void *userp) {
-    static int line = 0;
+    struct upload_status *upload_ctx = (struct upload_status *) userp;
     const char *data;
+    size_t room = size * nmemb;
 
     if (size == 0 || nmemb == 0 || (size * nmemb) < 1) {
         return 0;
     }
 
-    data = payload_text[line];
-    return -1;
+    data = payload_text[upload_ctx->bytes_read];
+    
+    if(data) {
+        size_t len = strlen(data);
+        if (room < len) {
+            len = room;
+        }
+        memcpy(ptr, data, len);
+        upload_ctx->bytes_read += len;
+
+        return len;
+    }
+
+    return 0;
 }
 
 void sendMail_send(char *sendTo) {
     CURL *curl;
     CURLcode res = CURLE_OK;
+    struct upload_status upload_ctx = { 0 };
     const char* user = getenv("GMAIL_USER");
     const char* pass = getenv("GMAIL_PASS");
 
@@ -45,6 +63,7 @@ void sendMail_send(char *sendTo) {
         recipients = curl_slist_append(recipients, sendTo);
         curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
         curl_easy_setopt(curl, CURLOPT_READFUNCTION, payload_source);
+        curl_easy_setopt(curl, CURLOPT_READDATA, &upload_ctx);
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
 
         res = curl_easy_perform(curl);
@@ -58,3 +77,4 @@ void sendMail_send(char *sendTo) {
         curl_easy_cleanup(curl);
     }
 }
+
