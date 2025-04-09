2af90da58d4b0c1cf32c198b425a9ab41a4aae5f
Nerv3sine <jasoncai64@gmail.com>
2025-04-09 03:05:16 -0700

    fixed audio recording and udp sending implementation

---
+++ app/include/audio_proc.h
@@ -0,0 +1,8 @@
+#ifndef _AUDIO_PROC_H_
+#define _AUDIO_PROC_H_
+
+void AudioProc_init();
+
+void AudioProc_cleanup();
+
+#endif
---
+++ app/src/audio_proc.c
@@ -0,0 +1,28 @@
+#include "audio_proc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <assert.h>
+#include <pthread.h>
+
+static int init = 0;
+static pthread_t thread;
+
+static void* audioProcess(){
+    assert(init);
+    while(init){
+        system("ffmpeg -hide_banner -loglevel error -ar 44100 -f alsa -i default:CARD=C920 -t 10 -acodec mp3 -f mp3 udp://192.168.7.2:12343");
+    }
+}
+
+void AudioProc_init(){
+    init = 1;
+    pthread_create(&thread, NULL, audioProcess, NULL);
+}
+
+void AudioProc_cleanup(){
+    init = 0;
+    pthread_join(thread, NULL);
+}
